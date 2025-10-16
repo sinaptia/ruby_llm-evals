cdad1236cab6dd5ab75dbc72d91b473bb6799fee
Patricio Mac Adden <patriciomacadden@gmail.com>
2025-10-16 11:18:45 -0300

    revert some stuff

--- lib/prompt_bench/engine.rb
+++ lib/prompt_bench/engine.rb
@@ -7,12 +7,9 @@ module PromptBench
     isolate_namespace PromptBench
 
     initializer "prompt_bench.assets" do |app|
-      # Add engine asset paths so Sprockets can find individual files
       app.config.assets.paths << root.join("app/assets/stylesheets")
       app.config.assets.paths << root.join("app/assets/images")
 
-      # Precompile individual CSS files instead of using manifest
-      # (avoids manifest processing issues when main app uses Vite)
       app.config.assets.precompile += %w[
         prompt_bench/application.css
         prompt_bench/bulma.min.css
