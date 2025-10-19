a8d333aefa80c52df87a233395791552fc0fdc6a
F-3r <F-3r@users.noreply.github.com>
2025-10-18 22:39:31 -0300

    Improve rails 7 support (#5)

--- lib/generators/prompt_bench/install/templates/db/migrate/create_prompt_bench_prompt_executions.rb
+++ lib/generators/prompt_bench/install/templates/db/migrate/create_prompt_bench_prompt_executions.rb
@@ -1,8 +1,8 @@
 class CreatePromptBenchPromptExecutions < ActiveRecord::Migration[<%= ActiveRecord::Migration.current_version %>]
   def change
     create_table :prompt_bench_prompt_executions do |t|
-      t.references :prompt_bench_eval_example, null: false, foreign_key: true
-      t.references :prompt_bench_eval_result, null: false, foreign_key: true
+      t.references :prompt_bench_eval_example, null: false, foreign_key: true, index: {name: 'index_pb_prompt_executions_on_pb_eval_example_id'}
+      t.references :prompt_bench_eval_result, null: false, foreign_key: true, index: {name: 'index_pb_prompt_executions_on_pb_eval_result_id'}
       t.string :eval_type, null: false
       t.text :expected_output
       t.json :variables
