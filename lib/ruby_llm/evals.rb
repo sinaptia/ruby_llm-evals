e64da6343477020a4a0cb616fa67242f5c88e65c
Patricio Mac Adden <patriciomacadden@gmail.com>
2025-10-23 23:27:35 -0300

    fix deprecation warnings

--- lib/ruby_llm/evals.rb
+++ lib/ruby_llm/evals.rb
@@ -7,8 +7,6 @@ require "ruby_llm/schema"
 
 module RubyLLM
   module Evals
-    include ActiveSupport::Configurable
-
-    config_accessor :importmap, default: Importmap::Map.new
+    mattr_accessor :importmap, default: Importmap::Map.new
   end
 end
