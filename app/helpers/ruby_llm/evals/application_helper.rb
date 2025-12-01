f01b7c077bc1c24e5c8f362ff74fae84ea656c3e
Patricio Mac Adden <patriciomacadden@gmail.com>
2025-12-01 14:40:12 -0300

    refactor stuff

--- app/helpers/ruby_llm/evals/application_helper.rb
+++ app/helpers/ruby_llm/evals/application_helper.rb
@@ -1,6 +1,11 @@
 module RubyLLM
   module Evals
     module ApplicationHelper
+      def json(hash)
+        if hash.present?
+          content_tag :pre, JSON.pretty_generate(hash)
+        end
+      end
     end
   end
 end
--- app/helpers/ruby_llm/evals/runs_helper.rb
+++ app/helpers/ruby_llm/evals/runs_helper.rb
@@ -1,6 +1,15 @@
 module RubyLLM
   module Evals
     module RunsHelper
+      def accuracy(run)
+        run.finished? ? "#{run.accuracy}%" : "N/A"
+      end
+
+      def duration(run)
+        return "N/A" unless run.finished?
+
+        distance_of_time_in_words run.started_at, run.ended_at, include_seconds: true
+      end
     end
   end
 end
