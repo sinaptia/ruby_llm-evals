9e9da0fa703da690e535d55df62b26fc4458c652
Patricio Mac Adden <patriciomacadden@gmail.com>
2025-12-17 17:28:23 -0300

    improve expected output in templates

--- app/helpers/ruby_llm/evals/application_helper.rb
+++ app/helpers/ruby_llm/evals/application_helper.rb
@@ -7,6 +7,14 @@ module RubyLLM
         distance_of_time_in_words obj.started_at, obj.ended_at, include_seconds: true
       end
 
+      def expected_output(obj)
+        if obj.regex?
+          content_tag :code, "/#{obj.expected_output}/i"
+        else
+          obj.expected_output
+        end
+      end
+
       def json(hash)
         if hash.present?
           content_tag :pre, JSON.pretty_generate(hash)
